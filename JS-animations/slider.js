d6445148b77e602931e744ac5a9b870f17561d73
Vuadens <jmaza809@gmail.com>
2025-11-17 00:24:37 -0300

    Ahora el slider funciona, habria que ver el tema de que las fotos queden mejor centradas

---
+++ JS-animations/slider.js
@@ -0,0 +1,30 @@
+const track = document.getElementById("img-track");
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+window.onmousemove = e => {
+    if(track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+          maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100;
+    const nextPercentage = parseFloat(track.dataset.prevPercentage || 0) + percentage;
+    const clampedPercentage = Math.min(Math.max(nextPercentage, -100), 0);
+
+    
+    track.dataset.percentage = clampedPercentage;
+         
+    track.style.transform = `translate(${clampedPercentage}%, 45%)`;
+
+    for(const image of track.getElementsByClassName("image")){
+        image.style.objectPosition = `${100 + clampedPercentage}% center`;
+    }
+}
